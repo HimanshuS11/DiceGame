da41dd77ccc1fe0ec90e5d3dca7cf02117ec9705
HimanshuS11 <123104397+HimanshuS11@users.noreply.github.com>
2023-05-03 19:07:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var randomNumber1=Math.round(Math.random()*6)+1;
+function c(){var randomNumber1=Math.round(Math.random()*6)+1;
 var randomimage1='dice'+randomNumber1+'.png';
 var randomNumber2=Math.round(Math.random()*6)+1;
 var randomimage2='dice'+randomNumber2+'.png';
@@ -10,4 +10,5 @@ else if(randomNumber1<randomNumber2)
 document.querySelector('h1').innerHTML='Player 2 Wins!! ';
 else
 document.querySelector('h1').innerHTML='Player 1 Wins!! ';
+}
 
